37fc3017132f4333c210e8eed8b2ffb5d2932b39
Amber <hello@ambercook.codes>
2016-09-05 13:09:39 -0400

    Updated gulp watch to reload page on more changes, started pulling together general page structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,40 @@
 var gulp        = require('gulp'),
-	browserSync = require('browser-sync').create(),
-    jekyll = require('gulp-jekyll'),
-	sass = require('gulp-sass');
+	browserSync = require('browser-sync'),
+    cp          = require('child_process'),
+	sass        = require('gulp-sass'),
+    jekyll      = 'jekyll';
 
 
 var reload  = browserSync.reload;
 
 gulp.task('sass', function() {
-    gulp.src(['./assets/scss/*.scss','./assets/scss/**/*.scss'])
+    gulp.src(['./assets/scss/style.scss'])
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./assets/'));
+        .pipe(gulp.dest('./assets/'))
+        .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        port: 8080,
-        open: false,
-        notify: false,
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    browserSync({
         server: {
-            baseDir: "./_site"
+            baseDir: '_site'
         }
     });
 });
 
-gulp.task('jekyll', function () {
-
-    gulp.src(['./index.html'])
-        .pipe(jekyll({
-            source: './',
-            bundleExec: true
-        }))
-        .pipe(gulp.dest('./_site/'));
-
+gulp.task('jekyll-build', function (done) {
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
 });
 
-gulp.task('default', ['browser-sync'], function () {
-
-    gulp.watch(['./index.html', './_includes/*.html'], ['jekyll', reload]);
-    gulp.watch('./assets/scss/**/*.scss',['sass', reload]);
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
 });
+
+
+gulp.task('watch', function () {
+    gulp.watch('./assets/scss/*.scss',['sass','jekyll-rebuild']);
+    gulp.watch(['./index.html', './_includes/*.html'], ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch' ]);
